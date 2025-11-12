47624a7bcfc9620e90e75b5036ea279f4adb58f9
funnbot <22226942+funnbot@users.noreply.github.com>
2025-11-12 13:31:04 -0800

    use the correct type LayoutProps for the return value of $props in a +layout.svelte script.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,9 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-	import type { Snippet } from 'svelte';
+	import type { LayoutProps } from './$types';
 
-	interface Props {
-		children: Snippet;
-	}
-
-	let { children }: Props = $props();
+	let { children }: LayoutProps = $props();
 </script>
 
 <svelte:head>
