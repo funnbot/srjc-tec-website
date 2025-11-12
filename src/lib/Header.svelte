e11a7982d172ee9b0e8e9a6db25e16842471b404
funnbot <22226942+funnbot@users.noreply.github.com>
2025-11-12 13:56:21 -0800

    feat(header): allow header tabs to be disabled

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import type { Pathname } from '$app/types';
 	import Button from './Button.svelte';
 
-	const tabs: string[] = ['About', 'Events', 'Team', 'Sponsors', 'Projects'];
+	interface Tab {
+		/** Display string for this tab. */
+		label: string;
+		/** Route to local page, used with `resolve()`. */
+		route: Pathname;
+		/** Should this button be disabled. */
+		disabled?: boolean;
+	}
+
+	const tabs: Tab[] = [
+		{ label: 'About', route: '/', disabled: true },
+		{ label: 'Events', route: '/', disabled: true },
+		{ label: 'Team', route: '/', disabled: true },
+		{ label: 'Sponsors', route: '/', disabled: true },
+		{ label: 'Projects', route: '/', disabled: true },
+	];
 </script>
 
 <header class="bg-header">
@@ -32,27 +48,42 @@
     ">
 				<nav aria-label="Main" class="block">
 					<ul class="flex size-full items-stretch text-sm">
-						{#each tabs as label (label)}
+						{#each tabs as { label, route, disabled } (label)}
 							<li>
-								<a
-									class="
-           flex h-full items-center px-1 text-nav-tab-label transition
-           hover:bg-nav-tab-hover
-           active:bg-nav-tab-hover
-           sm:px-2
-           md:px-4
-         "
-									href="#">
-									{label}
-								</a>
+								<!-- Properly disabling anchor elements is unnecessarily complicated, 
+								 use svelte to replace with a non interactive para element instead. -->
+								{#if !disabled}
+									<a
+										class="
+            flex h-full items-center px-1 text-nav-tab-label transition
+            hover:bg-nav-tab-hover
+            active:bg-nav-tab-hover
+            sm:px-2
+            md:px-4
+          "
+										href={resolve(route)}>
+										{label}
+									</a>
+								{:else}
+									<p
+										aria-disabled="true"
+										class="
+            flex h-full items-center px-1 text-on-primary-muted
+            sm:px-2
+            md:px-4
+          ">
+										{label}
+									</p>
+								{/if}
 							</li>
 						{/each}
 					</ul>
 				</nav>
-				<div class="
-      hidden self-center
-      sm:block
-    ">
+				<div
+					class="
+       hidden self-center
+       sm:block
+     ">
 					<Button href={resolve('/')}>Join Us</Button>
 				</div>
 			</div>
